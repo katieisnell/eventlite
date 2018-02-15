3228d920bc11e5970a806a77dcb28bb5199b7214
Adam Brock <adam.brock-2@student.manchester.ac.uk>
2018-02-15 12:39:24 +0000

    VenueRepository interface created

---
+++ src/main/java/uk/ac/man/cs/eventlite/dao/VenueRepository.java
@@ -0,0 +1,8 @@
+package uk.ac.man.cs.eventlite.dao;
+import org.springframework.data.repository.CrudRepository;
+
+import uk.ac.man.cs.eventlite.entities.Venue;
+
+public interface VenueRepository extends CrudRepository<Venue, Long> {
+
+}
