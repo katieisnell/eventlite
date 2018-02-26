30d3a8fcaf386125e9476ce9877cff105be73f07
Tong Li <tong.li-3@student.manchester.ac.uk>
2018-02-26 16:15:18 +0000

    Fix the problems, lost venues in merge

--- src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
@@ -34,24 +34,29 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
 			log.info("Database already populated. Skipping data initialization.");
 			return;
 		}
+		
+		Venue kilburn = new Venue();
+		kilburn.setName("Kilburn Building");
+		kilburn.setCapacity(200);
+		venueService.save(kilburn);
 
 		//Set dates to year + 1900, month, day, hour, minute
 		Event event1 = new Event();
 		event1.setName("COMP23412 Showcase, group G");
-		event1.setVenue(1);
+		event1.setVenue(kilburn);
 		event1.setDate(new Date(117, 10, 12, 11, 10));
 		event1.setTime(new Date(117, 10, 12, 11, 10));
 
 
 		Event event2 = new Event();
 		event2.setName("COMP23412 Showcase, group H");
-		event2.setVenue(1);
+		event2.setVenue(kilburn);
 		event2.setDate(new Date(117, 10, 12, 11, 10));
 		event2.setTime(new Date(105, 04, 1, 10, 10));
 
 		Event event3 = new Event();
 		event3.setName("COMP23412 Showcase, group F");
-		event3.setVenue(1);
+		event3.setVenue(kilburn);
 		event3.setDate(new Date(110, 03, 14, 5, 10));
 		event3.setTime(new Date(110, 03, 14, 5, 10));
 	
