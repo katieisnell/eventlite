f599780a606172698335ba5a757ff46f3cd8fc56
Jinpei Han <jinpei.han@student.manchester.ac.uk>
2018-03-06 13:01:36 +0000

    Removed tests for venues.findAll() Fixed cancel button on create event page

--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerTest.java
@@ -72,28 +72,28 @@ public class EventsControllerTest {
 	@Test
 	public void getIndexWhenNoEvents() throws Exception {
 		when(eventService.findAll()).thenReturn(Collections.<Event> emptyList());
-		when(venueService.findAll()).thenReturn(Collections.<Venue> emptyList());
+		//when(venueService.findAll()).thenReturn(Collections.<Venue> emptyList());
 
 		mvc.perform(get("/events").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
 				.andExpect(view().name("events/index")).andExpect(handler().methodName("getAllEvents"));
 
 		verify(eventService).findAll();
-		verify(venueService).findAll();
+		//verify(venueService).findAll();
 		verifyZeroInteractions(event);
-		verifyZeroInteractions(venue);
+		//verifyZeroInteractions(venue);
 	}
 
 	@Test
 	public void getIndexWithEvents() throws Exception {
 		when(eventService.findAll()).thenReturn(Collections.<Event> singletonList(event));
-		when(venueService.findAll()).thenReturn(Collections.<Venue> singletonList(venue));
+		//when(venueService.findAll()).thenReturn(Collections.<Venue> singletonList(venue));
 
 		mvc.perform(get("/events").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
 				.andExpect(view().name("events/index")).andExpect(handler().methodName("getAllEvents"));
 
 		verify(eventService).findAll();
-		verify(venueService).findAll();
+		//verify(venueService).findAll();
 		verifyZeroInteractions(event);
-		verifyZeroInteractions(venue);
+		//verifyZeroInteractions(venue);
 	}
 }
