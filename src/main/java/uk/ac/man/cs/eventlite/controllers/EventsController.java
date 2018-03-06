ff7dca1052cac8562b45f80b248efb566196815d
Tong Li <tong.li-3@student.manchester.ac.uk>
2018-03-06 22:16:51 +0000

    add methods for controller and controller API

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -1,13 +1,21 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 
 @Controller
 @RequestMapping(value = "/events", produces = { MediaType.TEXT_HTML_VALUE })
@@ -15,6 +23,10 @@ public class EventsController {
 
 	@Autowired
 	private EventService eventService;
+	
+	@Autowired
+	private VenueService venueService;
+	
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String getAllEvents(Model model) {
@@ -22,6 +34,33 @@ public class EventsController {
 		model.addAttribute("events", eventService.findAll());
 
 		return "events/index";
+	}
+	
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public String newEvent(Model model,@ModelAttribute("event")Event event) {
+		if (!model.containsAttribute("events")) {
+			model.addAttribute("events", new Event());
+		}
+		
+		model.addAttribute("venues", venueService.findAll());
+
+		return "events/new";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String createEvent(@RequestBody @Valid @ModelAttribute Event event,
+			BindingResult errors, Model model, RedirectAttributes redirectAttrs) {
+
+		if (errors.hasErrors()) {
+			model.addAttribute("events", event);
+			model.addAttribute("venues", venueService.findAll());
+			return "events/new";
+		}
+
+		eventService.save(event);
+		redirectAttrs.addFlashAttribute("ok_message", "New event added.");
+
+		return "redirect:/events";
 	}
 
 }
--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApi.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApi.java
@@ -3,15 +3,22 @@ package uk.ac.man.cs.eventlite.controllers;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +54,24 @@ public class EventsControllerApi {
 		}
 
 		return new Resources<Resource<Event>>(resources, selfLink);
+	}
+	
+	
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public ResponseEntity<?> newEvent() {
+		return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> createEvent(@RequestBody @Valid Event event, BindingResult result) {
+
+		if (result.hasErrors()) {
+			return ResponseEntity.unprocessableEntity().build();
+		}
+
+		eventService.save(event);
+		URI location = linkTo(EventsControllerApi.class).slash(event.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
 	}
 }
