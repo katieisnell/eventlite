d4d59721eef104358c72a34fa4029ef31eedd542
Jinpei Han <jinpei.han@student.manchester.ac.uk>
2018-04-10 12:33:15 +0100

    Fixed small problems caused by merging

--- src/main/java/uk/ac/man/cs/eventlite/dao/VenueRepository.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/VenueRepository.java
@@ -2,12 +2,11 @@ package uk.ac.man.cs.eventlite.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.entities.Venue;
 
 public interface VenueRepository extends CrudRepository<Venue, Long> {
 	
-	Optional<Venue> findById(long id);
+	Venue findById(long id);
 	Iterable<Venue> findByOrderByNameAsc();
 	
 }
